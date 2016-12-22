aa6e24e290aee1b2821a4cae8909df4f89f1dbb4
Lachezar Bozhkov <lachezar.bozhkov@gmail.com>
2016-12-22 15:37:18 +0200

    Add save img from simulator functionality

--- drive.py
+++ drive.py
@@ -1,6 +1,8 @@
 import argparse
 import base64
+from datetime import datetime
 import json
+import os
 
 import numpy as np
 import socketio
@@ -21,18 +23,34 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
+
+img_path_exists = os.path.exists("simulator_imgs")
+if img_path_exists:
+    simulator_imgs = open(r'simulator_imgs/data.csv', 'a')
+
+
+def save_img(image, steering_angle):
+    if img_path_exists:
+        file_path = datetime.now().strftime('simulator_imgs/%Y%m%d-%H%M%S-%f.jpg')
+        # cv2.imwrite(file_path, cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        cv2.imwrite(file_path, image)
+        simulator_imgs.write("{}, {}\n".format(file_path, steering_angle))
+
+
 def load_img(img, resize=True, crop_top=20, crop_bottom=-1):
     if resize:
-        img = cv2.resize(img,(160, 80), interpolation = cv2.INTER_CUBIC)
+        img = cv2.resize(img, (160, 80), interpolation=cv2.INTER_CUBIC)
     if crop_top:
-        img = img[crop_top:crop_bottom, :] 
-        
-    return img 
+        img = img[crop_top:crop_bottom, :]
+
+    return img
+
 
 def ld_img(img):
     # return load_img(img, resize=True, crop_top=30, crop_bottom=-10)
     # return load_img(img, resize=False, crop_top=60, crop_bottom=-15)
     return load_img(img, resize=False, crop_top=60, crop_bottom=-1)
+
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -45,15 +63,14 @@ def telemetry(sid, data):
     # The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
-    image_array = ld_img(image_array)
+    original_image = np.asarray(image)
+    image_array = ld_img(original_image)
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     steering_angle = max(min(steering_angle, 1), -1)
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    # throttle = 0.1 + max(0, 0.6 - abs(steering_angle*2))  # 0.2
-    throttle = 0.2
+    save_img(original_image, steering_angle)
+    throttle = 0.2  # Use 0.3 for the second track
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
